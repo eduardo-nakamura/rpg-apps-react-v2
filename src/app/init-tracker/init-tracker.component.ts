16be713ba86ea0ea7d77352a5fad738fb7a90032
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-02-19 09:29:00 -0300

    init tracker atualiza

--- src/app/init-tracker/init-tracker.component.ts
+++ src/app/init-tracker/init-tracker.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
+export class formChar {
+  name: string;
+  bonusInit: number = 0;
+  diceInit: number = 0;
+  totalInit: number = 0
+}
 @Component({
   selector: 'app-init-tracker',
   templateUrl: './init-tracker.component.html',
@@ -9,6 +15,8 @@ import { ApiService } from '../api.service';
 export class InitTrackerComponent implements OnInit {
   selectedParty;
   initObj:number
+  addNewCharInput = false
+  addNewChar = ''
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
@@ -17,19 +25,27 @@ export class InitTrackerComponent implements OnInit {
     //   this.selectedParty = data;
     // });
   }
+  addChar() {    
+    if(this.addNewChar != ''){
+      let teste = new formChar();
+      teste.name = this.addNewChar;
+      this.selectedParty.push(teste)
+      console.log('teste',teste,this.selectedParty)
+      this.addNewChar = ''
+    }
+  }
   selectChangeHandler(event: any) {    
     if(event.target.value == 0){
-      this.apiService.getNews().subscribe((data)=>{
-        console.log(data);
+      this.apiService.getNews().subscribe((data)=>{        
         this.selectedParty = data;
       });
     } else if(event.target.value == 1){
-      this.apiService.getNewss().subscribe((data)=>{
-        console.log(data);
+      this.apiService.getNewss().subscribe((data)=>{        
         this.selectedParty = data;
       });
     }
-    
+    this.addNewCharInput = true
+    this.atualizarTabela()
     // let teste = event.target.value;
     // console.log(event.target.value)
     // if(event.target.value == 0){
@@ -43,7 +59,15 @@ export class InitTrackerComponent implements OnInit {
     //   });
     // }
     // console.log(this.selectedParty)
-    // this.addNewCharInput=true
     // this.atualizarTabela()
+  }
+  atualizarTabela() {    
+    
+    // if(this.initObj[0].name == ''){
+    //   this.initObj.splice(0, 1)
+    // }    
+    // for (let i = 0; i < this.initObj.length; i++) {
+    //   this.initObj[i].totalInit = this.initObj[i].bonusInit + this.initObj[i].diceInit
+    // }    
   }
 }
