77d2b66ce8bb4ec26cff8c5b6d431bbaddc6c594
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-02-27 16:41:26 -0300

    build

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,45 +7,45 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
   constructor(private httpClient: HttpClient) { }
   public getFahen(){      
-    return this.httpClient.get(`../assets/mock/fahen.json`);
+    return this.httpClient.get(`../rpg/assets/mock/fahen.json`);
   }
   public getLytix(){  
-    return this.httpClient.get(`../assets/mock/lytix.json`);
+    return this.httpClient.get(`../rpg/assets/mock/lytix.json`);
   }
   public getGameHooks(){  
-    return this.httpClient.get(`../assets/mock/hooks.json`);
+    return this.httpClient.get(`../rpg/assets/mock/hooks.json`);
   }
   public getLootIndividual0to4(){
-    return this.httpClient.get(`../assets/mock/individual0to4.json`);
+    return this.httpClient.get(`../rpg/assets/mock/individual0to4.json`);
   }
   public getLootIndividual5to10(){
-    return this.httpClient.get(`../assets/mock/individual5to10.json`);
+    return this.httpClient.get(`../rpg/assets/mock/individual5to10.json`);
   }
   public getLootIndividual11to16(){
-    return this.httpClient.get(`../assets/mock/individual11to16.json`);
+    return this.httpClient.get(`../rpg/assets/mock/individual11to16.json`);
   }
   public getLootIndividual17plus(){
-    return this.httpClient.get(`../assets/mock/individual17plus.json`);
+    return this.httpClient.get(`../rpg/assets/mock/individual17plus.json`);
   }
   public getLootHoard0to4(){
-    return this.httpClient.get(`../assets/mock/hoard0to4.json`);
+    return this.httpClient.get(`../rpg/assets/mock/hoard0to4.json`);
   }
   public getLootHoard5to10(){
-    return this.httpClient.get(`../assets/mock/hoard5to10.json`);
+    return this.httpClient.get(`../rpg/assets/mock/hoard5to10.json`);
   }
   public getLootHoard11to16(){
-    return this.httpClient.get(`../assets/mock/hoard11to16.json`);
+    return this.httpClient.get(`../rpg/assets/mock/hoard11to16.json`);
   }
   public getLootHoard17plus(){
-    return this.httpClient.get(`../assets/mock/hoard17plus.json`);
+    return this.httpClient.get(`../rpg/assets/mock/hoard17plus.json`);
   }
   public gems(){
-    return this.httpClient.get(`../assets/mock/gems.json`)
+    return this.httpClient.get(`../rpg/assets/mock/gems.json`)
   }
   public art(){
-    return this.httpClient.get(`../assets/mock/art.json`)
+    return this.httpClient.get(`../rpg/assets/mock/art.json`)
   }
   public magItem(){
-    return this.httpClient.get(`../assets/mock/magItem.json`)
+    return this.httpClient.get(`../rpg/assets/mock/magItem.json`)
   }
 }
