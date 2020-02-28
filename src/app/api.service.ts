59a5a16216299849e36a92a14de0e78e8a50c10c
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-02-28 17:47:11 -0300

    dice froller

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,122 +1,61 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// switch(window.location.host) {
+//   case 'localhost:4200':
+//     let serve2 = "../assets/mock/"
+//     break;
+//   default:
+//     serve2 = "../rpg/assets/mock/"
+// }
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
-  constructor(private httpClient: HttpClient) { }
+export class ApiService {  
+  
+  serve = "../assets/mock/"
+  serve2 = "../rpg/assets/mock/"
+  constructor(private httpClient: HttpClient) { }  
   public getFahen(){      
-    
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/fahen.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/fahen.json`);
-    }
-    // return this.httpClient.get(`../rpg/assets/mock/fahen.json`);
+    return this.httpClient.get(`${this.serve}fahen.json`);
   }
   public getLytix(){  
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/lytix.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/lytix.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}lytix.json`);    
   }
   public getGameHooks(){  
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/hooks.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/hooks.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}hooks.json`);
   }
-  public getLootIndividual0to4(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/individual0to4.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/individual0to4.json`);
-    }
-    
+  public getLootIndividual0to4(){    
+    return this.httpClient.get(`${this.serve}individual0to4.json`);
   }
   public getLootIndividual5to10(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/individual5to10.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/individual5to10.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}individual5to10.json`);
   }
   public getLootIndividual11to16(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/individual11to16.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/individual11to16.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}individual11to16.json`);
   }
   public getLootIndividual17plus(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../rpg/assets/mock/individual17plus.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/individual17plus.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}individual17plus.json`);
   }
   public getLootHoard0to4(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/hoard0to4.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/hoard0to4.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}hoard0to4.json`);
   }
   public getLootHoard5to10(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/hoard5to10.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/hoard5to10.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}hoard5to10.json`);
   }
   public getLootHoard11to16(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/hoard11to16.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/hoard11to16.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}hoard11to16.json`);
   }
   public getLootHoard17plus(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/hoard17plus.json`);
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/hoard17plus.json`);
-    }
-    
+    return this.httpClient.get(`${this.serve}hoard17plus.json`);
   }
   public gems(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/gems.json`)
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/gems.json`)
-    }
-    
+    return this.httpClient.get(`${this.serve}gems.json`)
   }
   public art(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/art.json`)
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/art.json`)
-    }
-    
+    return this.httpClient.get(`${this.serve}art.json`)
   }
   public magItem(){
-    if(window.location.host == "localhost:4200"){
-      return this.httpClient.get(`../assets/mock/magItem.json`)
-    } else {
-      return this.httpClient.get(`../rpg/assets/mock/magItem.json`)
-    }
-    
+    return this.httpClient.get(`${this.serve}magItem.json`)
   }
 }
